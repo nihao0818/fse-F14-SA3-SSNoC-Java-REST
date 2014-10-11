58bb73e489bc852f8f89f3980745801d85a2ed61
Tangent Chang <tien.jen.chang@sv.cmu.edu>
2014-10-10 18:12:59 -0700

    no

--- src/it/java/edu/cmu/sv/ws/ssnoc/test/SocialNetworkAnalysisTest.java
+++ src/it/java/edu/cmu/sv/ws/ssnoc/test/SocialNetworkAnalysisTest.java
@@ -2,16 +2,23 @@ package edu.cmu.sv.ws.ssnoc.test;
 
 import static org.junit.Assert.*;
 
+import edu.cmu.sv.ws.ssnoc.data.po.ExchangeInfoPO;
 import org.junit.Before;
 import org.junit.Test;
 /**
  * Created by YHWH on 10/10/14.
  */
 public class SocialNetworkAnalysisTest {
+    ExchangeInfoPO userA, userB, userC, userD;
+
     @Before
-    public void one(){}
+    public void setUp() throws Exception{
+        userA =
+    }
 
     @Test
-    public void two(){}
+    public void test() {
+
+    }
 
 }
